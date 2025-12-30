5d919a79378912a09755fac1d1e4df1e1c08dec9
navneethKrishnan <72127953+navneethKrishnan@users.noreply.github.com>
2025-12-31 02:51:35 +0530

    Refactor audio and stage management logic

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ function initMoments() {
         "20250609_185119.jpg",
         "20250706_154925.jpg",
         "20250706_154959.jpg",
-        "_DSC6021.jpg",
+        "_DSC6021.JPG",
         "IMG_20241111_184837.jpg",
         "20250912_122210.jpg",
         "20250912_132703.jpg",
@@ -284,3 +284,4 @@ const greetingCard = document.getElementById('greeting-card');
 greetingCard.addEventListener('click', () => {
     greetingCard.classList.toggle('open');
 });
+
